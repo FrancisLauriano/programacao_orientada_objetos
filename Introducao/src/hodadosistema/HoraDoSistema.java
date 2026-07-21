fc8f39fb57ae39c0aa87d694c273656d533d507a
FrancisLauriano <francislauriano@gmail.com>
2026-07-21 02:12:04 +0100

    organizando

---
+++ Introducao/src/hodadosistema/HoraDoSistema.java
@@ -0,0 +1,13 @@
+package hodadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("O horario do sistema e:");
+        System.out.println(relogio.toString());
+    }
+    
+}
