8d8890ec05999abb7c1170fec3e7c839d78c8149
FrancisLauriano <francislauriano@gmail.com>
2026-08-15 23:23:14 -0300

    aula 02 - Objetos e Classes

---
+++ Aula02/Main.java
@@ -0,0 +1,40 @@
+package Aula02;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args){
+        double x1, x2, y1, y2, distanciaAB;
+        Scanner entrada = new Scanner(System.in);
+
+        
+    
+        System.out.print("Informe X1: ");
+        x1 = entrada.nextDouble();
+
+        System.out.print("Informe Y1: ");
+        y1 = entrada.nextDouble();
+
+        Ponto p1 = new Ponto(x1, y1);
+
+
+        System.out.print("Informe X2: ");
+        x2 = entrada.nextDouble();
+
+        System.out.print("Informe Y2: ");
+        y2 = entrada.nextDouble();
+
+        Ponto p2 = new Ponto(x2, y2);
+            
+
+        distanciaAB = p1.distanciaEntrePontos(p2);
+    
+
+        System.out.println("A distancia entre A e B é: " + distanciaAB);
+
+        entrada.close();
+        
+    }
+    
+}
---
+++ Aula02/Ponto.java
@@ -0,0 +1,23 @@
+package Aula02;
+import java.lang.Math;
+
+public class Ponto{
+    public double x;
+    public double y;
+
+    public Ponto(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public double distanciaEntrePontos(Ponto pB){
+        double distanciaAB;
+
+        distanciaAB = Math.sqrt(Math.pow((pB.x - this.x), 2) + Math.pow((pB.y - this.y), 2)) ;
+
+        return distanciaAB;
+
+    }
+
+}
